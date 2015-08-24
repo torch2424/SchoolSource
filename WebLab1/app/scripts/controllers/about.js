50914a5afd0e9b77bc5f76df73ce06e62d317fc6
Aaron <torch2424@hotmail.com>
2015-08-24 13:21:14 -0700

    instead generated an angular project for sass and responsiveness and things

---
+++ WebLab1/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webLab1App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webLab1App
+ */
+angular.module('webLab1App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ WebLab1/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webLab1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webLab1App
+ */
+angular.module('webLab1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
