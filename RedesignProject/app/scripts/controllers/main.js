d0f03d71224087aac1b1b8d5de2b3f7af10c2b20
Aaron <torch2424@hotmail.com>
2015-09-14 13:47:07 -0700

    Finished my first two pages

--- RedesignProject/app/scripts/controllers/main.js
+++ RedesignProject/app/scripts/controllers/main.js
@@ -8,10 +8,27 @@
  * Controller of the redesignProjectApp
  */
 angular.module('redesignProjectApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
+    ];
+
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    var slides = $scope.slides = [
+        {
+          image: 'http://lorempixel.com/400/200/'
+        },
+        {
+          image: 'http://lorempixel.com/400/200/food'
+        },
+        {
+          image: 'http://lorempixel.com/400/200/sports'
+        },
+        {
+          image: 'http://lorempixel.com/400/200/people'
+        }
     ];
   });
