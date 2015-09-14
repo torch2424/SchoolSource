e8596c1863fc079e5c656a483544c024dad7b1c2
Aaron <torch2424@hotmail.com>
2015-09-14 12:48:44 -0700

    Generated the redesign project

---
+++ RedesignProject/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name redesignProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the redesignProjectApp
+ */
+angular.module('redesignProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ RedesignProject/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name redesignProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the redesignProjectApp
+ */
+angular.module('redesignProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
